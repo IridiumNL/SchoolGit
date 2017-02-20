07517bc306e2a511d269dcaada6251b3bf1470e7
IridiumNL <randy.parsons32@ed.cna.nl.ca>
2017-02-20 16:18:48 -0330

    ayyy got some stuff done lol

---
+++ WorkingArea/JAVA/TestQueue.java
@@ -0,0 +1,7 @@
+class TestQueue {
+    public static void main(String[] args) {
+        Queue q2 = new Queue();
+        q2.enqueue(1); q2.enqueue(3); q2.enqueue(2);
+        System.out.println(q2);
+    }
+}
---
+++ WorkingArea/JAVA/TestQueueB.java
@@ -0,0 +1,10 @@
+class TestQueueB {
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        q.enqueue(1); q.enqueue(2);
+        System.out.println(q);
+        q.dequeue(); System.out.println(q);
+        q.dequeue(); System.out.println(q);
+        q.dequeue(); System.out.println(q);
+    }
+}
